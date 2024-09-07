260ee7eeb4f588b4375cd2e1d7ee7dbb2ebe9ba5
Arunabha-NOTE <arunabham2017@gmail.com>
2024-09-08 01:59:57 +0530

    saving changes

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-bio',
   standalone: true,
-  imports: [],
+    imports: [FormsModule, CheckboxModule],
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.css'
 })
--- src/app/stepper/stepper/stepper.component.ts
+++ src/app/stepper/stepper/stepper.component.ts
@@ -5,15 +5,22 @@ import {BioComponent} from "../../components/bio/bio.component";
 import {PsyComponent} from "../../components/psy/psy.component";
 import {ChatbotComponent} from "../../components/chatbot/chatbot.component";
 import {VideoComponent} from "../../components/video/video.component";
+import {CheckboxModule} from "primeng/checkbox";
+import {FormsModule} from "@angular/forms";
+import {InstructionsComponent} from "../../components/instructions/instructions.component";
 
 
 @Component({
-  selector: 'app-stepper',
-  standalone: true,
-    imports: [ButtonModule, StepperModule, BioComponent, PsyComponent, ChatbotComponent, VideoComponent],
-  templateUrl: './stepper.component.html',
-  styleUrl: './stepper.component.css'
+    selector: 'app-stepper',
+    standalone: true,
+    imports: [ButtonModule, StepperModule, BioComponent, PsyComponent, ChatbotComponent, VideoComponent, CheckboxModule, FormsModule, InstructionsComponent],
+    templateUrl: './stepper.component.html',
+    styleUrl: './stepper.component.css'
 })
 export class StepperComponent {
-
+    checked: boolean | undefined = false;
+    disabled: Boolean | undefined = true;
+    onCheckboxChange() {
+        this.disabled = !this.checked;
+    }
 }
