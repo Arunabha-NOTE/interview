8a27515c02080aaa315a1e9b478ee6435a3c69c5
Arunabha-NOTE <arunabham2017@gmail.com>
2024-09-08 02:18:33 +0530

    added the instructions.component.html

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox';
+import {InstructionsComponent} from "../instructions/instructions.component";
 
 @Component({
   selector: 'app-bio',
   standalone: true,
-    imports: [FormsModule, CheckboxModule],
+    imports: [FormsModule, CheckboxModule, InstructionsComponent],
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.css'
 })
